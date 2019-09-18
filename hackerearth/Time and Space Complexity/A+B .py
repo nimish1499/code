d1e89e797d0e1833bd4d1bbebdf1e255a7a166ac
nimish1499 <55277101+nimish1499@users.noreply.github.com>
2019-09-18 23:46:18 +0530

    Update A+B .py

--- hackerearth/Time and Space Complexity/A+B .py
+++ hackerearth/Time and Space Complexity/A+B .py
@@ -1,11 +1,11 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Nimish/AppData/Local/Programs/Python/Python37-32/hash.py =
-abcdeeedsd
-a1b1c1d3e3s1
->>> 
-= RESTART: C:/Users/Nimish/AppData/Local/Programs/Python/Python37-32/hash.py =
-aabcc
-a2b1c2
->>> 
+c = 0    
+for i in range(0, 13):
+   try:
+    m, n = input().split()
+    mo = int(m)
+    no = int(n)
+    if (0 <= no) & (mo <= (10 ** 98)):
+        c = mo + no
+        print(c)
+   except: 
+    break
