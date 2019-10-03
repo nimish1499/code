50d57e6513e52a4f94c75ef82a80a81e3de8d917
nimish1499 <nimishbahuguna999@gmail.com>
2019-10-03 15:42:50 +0530

    recent changes

---
+++ hackerearth/basic of IO/Seating Arrangement.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    s = n % 12
+    if s == 0:
+        n -= 11
+    elif s < 7:
+        n += (2 * (6 - s) + 1)
+    else:
+        n -= (2 * (s - 7) + 1)
+    r = n % 6
+    if r == 1 or r == 0:
+        print(n, "WS")
+    elif r == 2 or r == 5:
+        print(n, "MS")
+    else:
+        print(n, "AS")
