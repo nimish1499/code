d6ad8f209a1b7dd51a237a2d7c96df0fc1effe94
nimish1499 <nimishbahuguna999@gmail.com>
2019-10-08 21:55:33 +0530

    recent changes

---
+++ hackerearth/basic of IO/Conject-it.py
@@ -0,0 +1,12 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    while n != 1:
+        if n % 2 == 0:
+            n = n//2
+        else:
+            n = 3*n + 1
+    if n == 1:
+        print("YES")
+    else:
+        print("NO")
