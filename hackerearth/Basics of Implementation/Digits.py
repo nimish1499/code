ec1d1090ef10c9b44417048629e31fb55ebf1f5f
nimish1499 <nimishbahuguna999@gmail.com>
2019-10-17 21:44:41 +0530

    recent changes

--- hackerearth/Basics of Implementation/Digits.py
+++ hackerearth/Basics of Implementation/Digits.py
@@ -1,15 +1,15 @@
-sample,numberofdig = input().split()
-data = list(map(int,sample))
+sample, numberofdig = input().split()
+data = list(map(int, sample))
 size = int(len(data))
 n = int(numberofdig)
 times = 0
 
-for i in range(0,size,1):
+for i in range(0, size, 1):
     if times == n:
         break
     elif data[i] != 9:
         data[i] = 9
-        times+=1
+        times += 1
 
-num = int(''.join(map(str,data))) 
+num = int(''.join(map(str, data)))
 print(num)
