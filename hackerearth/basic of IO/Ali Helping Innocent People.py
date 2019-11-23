6d7b755f7fb402dca9a9ec1d53ac022b279a4b0d
nimish1499 <nimishbahuguna999@gmail.com>
2019-11-23 23:29:52 +0530

    recent changes

--- hackerearth/basic of IO/Ali Helping Innocent People.py
+++
@@ -1,6 +0,0 @@
-a = input()
-if (ord(a[0]) + ord(a[1])) % 2 == 0 and (ord(a[3]) + ord(a[4])) % 2 == 0 and (ord(a[5]) + ord(a[4])) % 2 == 0 and (
-        ord(a[7]) + ord(a[8])) % 2 == 0 and a[2] not in ("A", "E", "I", "O", "U", "Y"):
-    print("valid")
-else:
-    print("invalid")
