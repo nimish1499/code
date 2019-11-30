e541267e510f54c545b910634bfb3fb620eae5fd
nimish1499 <nimishbahuguna999@gmail.com>
2019-11-30 23:20:10 +0530

    recent changes

---
+++ hackerearth/Maths/Anshuli and Party.py
@@ -0,0 +1,8 @@
+import math
+T=int(input())
+while T:
+    a,b=list(map(int,input().split()))
+    print (pow(a,b,1000000007))
+    T -= 1
+    
+#https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-1/practice-problems/algorithm/sheero-and-the-party/#
