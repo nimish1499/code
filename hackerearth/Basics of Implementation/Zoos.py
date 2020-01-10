8a21da1c48f7d090d815d66c9a2a89935d1fdae5
nimish1499 <nimishbahuguna999@gmail.com>
2020-01-10 23:23:06 +0530

    recent changes

---
+++ hackerearth/Basics of Implementation/Zoos.py
@@ -0,0 +1,9 @@
+s =input()
+if len(s) < 21:
+    if (2*s.count('z')) == s.count('o'):
+        print("Yes")
+    else:
+        print("No")
+else:
+    print("No")
+#https://www.hackerearth.com/challenges/competitive/november-circuits-19/algorithm/is-zoo-f6f309e7/#
